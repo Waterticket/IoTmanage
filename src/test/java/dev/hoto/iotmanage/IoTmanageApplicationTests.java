f5a860d56ad12843d194bd49561f0496ed250365
Waterticket <matthew218@naver.com>
2020-12-10 13:16:49 +0900

    Initial commit

---
+++ src/test/java/dev/hoto/iotmanage/IoTmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hoto.iotmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
