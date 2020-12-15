f4657b8250f4739d858b377df23b6a9c47299bc6
biso <bi-so@naver.com>
2020-12-15 22:01:04 +0900

    register -> show 넘어가게 수정

--- src/main/java/dev/hoto/iotmanage/controller/DeviceController.java
+++ src/main/java/dev/hoto/iotmanage/controller/DeviceController.java
@@ -1,9 +1,13 @@
 package dev.hoto.iotmanage.controller;
 
+import dev.hoto.iotmanage.IoTDevice;
 import dev.hoto.iotmanage.service.deviceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class DeviceController {
@@ -11,10 +15,26 @@ public class DeviceController {
     deviceServiceImpl deviceService;
 
     @GetMapping("/register")
-    public String registerDevice() { return  "registerDevice"; }
+    public String registerDevice() {
+        return "registerDevice";
+    }
+
+    @GetMapping("/register/submit")
+    public String saveRegistered(HttpServletRequest request) {
+        String moduleType = request.getParameter("module_type");
+        String moduleName = request.getParameter("module_name");
+        String moduleOwner = request.getParameter("module_owner");
+
+        System.out.println(moduleType + ", " + moduleName + ", " + moduleOwner);
+
+
+        return "showDevice";
+    }
 
     @GetMapping("/show")
     public String showDevices() {
+
+        System.out.println("showDevices");
         return "showDevice";
     }
 
@@ -22,4 +42,6 @@ public class DeviceController {
     public String editDevices() {
         return "editDevice";
     }
+
+
 }
